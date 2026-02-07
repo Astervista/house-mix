b6f820483c5506d48fb8f3940fd4291415849640
Astervista <astervista@gmail.com>
2026-02-07 17:43:55 +0100

    Frontend: icons + splash screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, isDevMode} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {DynamicSvgComponent} from './components/auxiliary/dynamic-svg/dynamic-svg.component';
 
 @Component({
                selector:    'house-mix-root',
-               imports:     [RouterOutlet],
+               imports: [RouterOutlet, DynamicSvgComponent],
                templateUrl: './app.component.html',
                styleUrl:    './app.component.scss'
            })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     public title = 'HouseMix';
+
+    public ngAfterViewInit(): void {
+        if (typeof window !== 'undefined') {
+            if (isDevMode()) {
+                (window as unknown as {killSplash: () => void}).killSplash();
+            }
+        }
+    }
+
 }
