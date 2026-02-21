1fda7bf7c047ce38a1b986cbcc21196552357cb3
Astervista <astervista@gmail.com>
2026-02-21 01:21:49 +0100

    Small edit in the bundle start script and example env file

--- containers/bundle/start.sh
+++ containers/bundle/start.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 set -e
 
-GITHUB_TOKEN=$(cat ./secrets/github_token.txt)
+# Required environment variables / files
+REQUIRED_ENV_FILE=".env"
+REQUIRED_SECRETS="/run/secrets/github_token /run/secrets/mqtt_password"
 
+# Check if .env exists
+if [ ! -f "$REQUIRED_ENV_FILE" ]; then
+  echo "Error: Environment file '$REQUIRED_ENV_FILE' is missing! If you don't know what its contents should be, copy example.env and edit its values."
+  exit 1
+fi
+
+# Check if each secret exists
+for secret in $REQUIRED_SECRETS; do
+  if [ ! -f "$secret" ]; then
+    echo "Error: Required secret '$secret' is missing!"
+    exit 1
+  fi
+done
+
+GITHUB_TOKEN=$(cat ./secrets/github_token.txt)
 
 echo "$GITHUB_TOKEN"
 
